1fb31dde0b48486014c4ba142311892a1284fe25
daskhatri <blubd.softtech@gmail.com>
2019-12-10 20:43:02 +0500

    Commit to add the file: OrangeHrmTC1.java

---
+++ src/test/java/com/finexus/tests/OrangeHrmTC1.java
@@ -0,0 +1,81 @@
+package com.finexus.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class OrangeHrmTC1 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testOrangeHrmTC1() throws Exception {
+    driver.get("https://opensource-demo.orangehrmlive.com/");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Panel de inicio de sesión'])[1]/following::span[1]")).click();
+    driver.findElement(By.id("txtUsername")).clear();
+    driver.findElement(By.id("txtUsername")).sendKeys("admin");
+    driver.findElement(By.id("txtPassword")).clear();
+    driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+    driver.findElement(By.id("btnLogin")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Tiempo'])[1]/following::b[1]")).click();
+    driver.findElement(By.id("candidateSearch_jobVacancy")).click();
+    driver.findElement(By.id("candidateSearch_keywords")).click();
+    driver.findElement(By.id("candidateSearch_keywords")).clear();
+    driver.findElement(By.id("candidateSearch_keywords")).sendKeys("asdewq");
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
