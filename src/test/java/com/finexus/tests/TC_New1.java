f3114e2303cb931f49c868298f1f4a2b306ffc30
daskhatri <blubd.softtech@gmail.com>
2019-12-16 20:42:19 +0500

    Commit to add the file: TC_New1.java

---
+++ src/test/java/com/finexus/tests/TC_New1.java
@@ -0,0 +1,77 @@
+package com.finexus.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TCNew1 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTCNew1() throws Exception {
+    driver.get("https://www.kttape.com/kt-tape-products");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Out of stock'])[1]/following::img[1]")).click();
+    driver.findElement(By.id("product-addtocart-button")).click();
+    driver.findElement(By.linkText("View and Edit Cart")).click();
+    driver.findElement(By.id("cart-283617-qty")).click();
+    driver.findElement(By.id("cart-283617-qty")).clear();
+    driver.findElement(By.id("cart-283617-qty")).sendKeys("2");
+    // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
