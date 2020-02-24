854d4212dd0173c2a17229c6f855f9c30b12f612
daskhatri <blubd.softtech@gmail.com>
2020-02-24 16:48:49 +0500

    Commit to add the file: T24SearchTc.java

---
+++ src/test/java/com/finexus/tests/T24SearchTc.java
@@ -0,0 +1,107 @@
+package com.finexus.tests; 
+ import io.github.bonigarcia.wdm.WebDriverManager; 
+ import java.util.ArrayList;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class T24SearchTc {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    WebDriverManager.chromedriver().setup(); driver = new ChromeDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testT24SearchTc() throws Exception {
+    driver.get("https://www.google.com/search?q=T24+temnos&oq=T24+temnos&aqs=chrome..69i57j0l5.6776j0j7&sourceid=chrome&ie=UTF-8");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Showing results for'])[1]/following::i[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Web results'])[1]/following::h3[1]")).click();
+    driver.findElement(By.linkText("Products")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Temenos Infinity is a digital banking product focused on customer experience.'])[1]/following::span[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Temenos Data & Analytics'])[1]/following::div[1]")).click();
+    driver.findElement(By.linkText("Insights")).click();
+    driver.findElement(By.linkText("Delivery")).click();
+    driver.findElement(By.linkText("Solutions")).click();
+    driver.findElement(By.linkText("Contact Us")).click();
+    driver.close();
+  }
+
+public void selectFrame(int index) {
+		try {
+			Thread.sleep(5000);
+			driver.switchTo().frame(index);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void selectParentFrame() {		
+		driver.switchTo().parentFrame();
+	}
+
+	public void selectWindow(int index) {
+		try {
+			Thread.sleep(5000);
+			ArrayList<String> productCatalogueWindow = new ArrayList<String>(driver.getWindowHandles());
+			// change focus to new tab
+			driver.switchTo().window(productCatalogueWindow.get(index));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
