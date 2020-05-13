f8849d3488a4417cd67ae0bd42fb9ceafb66e6ea
daskhatri <blubd.softtech@gmail.com>
2020-05-13 17:18:58 +0500

    Commit to add the file: LoginAccountTC.java

---
+++ src/test/java/com/finexus/tests/LoginAccountTC.java
@@ -0,0 +1,115 @@
+package com.finexus.tests; 
+ import io.github.bonigarcia.wdm.WebDriverManager; 
+ import java.util.ArrayList;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LoginAccountTC {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    WebDriverManager.chromedriver().setup(); driver = new ChromeDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testLoginAccountTC() throws Exception {
+    driver.get("http://52.220.141.104:8080/BrowserWeb/servlet/BrowserServlet");
+    driver.findElement(By.id("signOnName")).click();
+    driver.findElement(By.id("signOnName")).clear();
+    driver.findElement(By.id("signOnName")).sendKeys("ALI001");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("testdemot");
+    driver.findElement(By.name("login")).submit();
+	 selectFrame(1);
+
+    driver.findElement(By.id("imgError")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Alerts Management'])[1]/following::span[1]")).click();
+    driver.findElement(By.linkText("Product Catalog")).click();
+	 selectWindow(1 );
+	 selectFrame(2);
+
+    driver.findElement(By.id("treestop1")).click();
+    driver.findElement(By.id("treestart1")).click();
+    driver.findElement(By.id("treestop1")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Current Accounts'])[1]/following::img[1]")).click();
+  }
+
+public void selectFrame(int index) {
+		try {
+			Thread.sleep(5000);
+			driver.switchTo().frame(index);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void selectParentFrame() {		
+		driver.switchTo().parentFrame();
+	}
+
+	public void selectWindow(int index) {
+		try {
+			Thread.sleep(5000);
+			ArrayList<String> productCatalogueWindow = new ArrayList<String>(driver.getWindowHandles());
+			// change focus to new tab
+			driver.switchTo().window(productCatalogueWindow.get(index));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
