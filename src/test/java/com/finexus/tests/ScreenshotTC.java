1335a21d29b664142332e76c7d23d479f46850de
daskhatri <blubd.softtech@gmail.com>
2020-06-01 23:06:52 +0500

    Commit to add the file: ScreenshotTC.java

---
+++ src/test/java/com/finexus/tests/ScreenshotTC.java
@@ -0,0 +1,122 @@
+package com.finexus.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.ArrayList;import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ScreenshotTC {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    WebDriverManager.chromedriver().setup(); driver = new ChromeDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testScreenshotTC() throws Exception {
+    driver.get("https://www.everlast.com/");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Menu'])[1]/following::span[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Boxing'])[1]/following::span[2]")).click();
+    driver.findElement(By.linkText("1910 Classic Training Gloves")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Color'])[1]/following::div[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Size'])[1]/following::div[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Quantity'])[1]/following::span[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Create Account'])[2]/following::a[1]")).click();
+    driver.findElement(By.id("top-cart-btn-checkout")).click();
+  }
+
+
+public void selectFrame(int index) {
+		try {
+			Thread.sleep(5000);
+			driver.switchTo().frame(index);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void selectParentFrame() {		
+		driver.switchTo().parentFrame();
+	}
+
+	public void selectWindow(int index) {
+		try {
+			Thread.sleep(5000);
+			ArrayList<String> productCatalogueWindow = new ArrayList<String>(driver.getWindowHandles());
+			// change focus to new tab
+			driver.switchTo().window(productCatalogueWindow.get(index));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+@AfterMethod
+  public void takeScreenShotOnFailure(ITestResult testResult) throws IOException {
+		if (testResult.getStatus() == ITestResult.FAILURE) {
+			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(scrFile, new File("errorScreenshots\\" + testResult.getName() + "-"
+					+ Arrays.toString(testResult.getParameters()) + ".jpg"));
+		}
+  }		@AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
